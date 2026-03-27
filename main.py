ce94437124170a302f094f114267b2791bdad640
Edima <edimaudo@gmail.com>
2026-03-27 05:08:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,6 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-# The Custom 404 Handler
-@app.exception_handler(404)
-async def custom_404_handler(request: Request, __):
-    # If the URL starts with /admin, give them the technical 404
-    if request.url.path.startswith("/admin"):
-        return templates.TemplateResponse("404_admin.html", {
-            "request": request
-        }, status_code=404)
-    
-    # Otherwise, give the public the reassurance 404
-    return templates.TemplateResponse("404_user.html", {
-        "request": request
-    }, status_code=404)
-
 # Landing page
 @app.get("/", response_class=HTMLResponse)
 async def landing_page(request: Request, lang: str = "en"):
